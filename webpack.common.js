0daec869188c672fa1660da4a5c71af5a59adf9b
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-09-25 03:07:27 +0500

    refactor(build): convert webpack configuration and eslint setup to ES modules
    - Update webpack configuration files to use ES module syntax
    - Replace require() with import statements
    - Convert module.exports to export/default export
    - Update eslint.config.mjs to use ES module import syntax

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,26 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import resolve from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-  entry: "./src/main.js",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/main.html",
-    }),
+export const entry = "./src/main.js";
+export const output = {
+  filename: "index.js",
+  path: resolve(__dirname, "dist"),
+  clean: true,
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/main.html",
+  }),
+];
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    },
+    {
+      test: /\.html$/i,
+      loader: "html-loader",
+    },
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-    ],
-  },
 };
