e9bfe21781d462ace7a1e8b4dc17c2834f0de947
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-09-25 03:02:37 +0500

    build(webpack): update output filename to index.js
    - Change output filename from 'main.js' to 'index.js' in webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/main.js",
   output: {
-    filename: "main.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
